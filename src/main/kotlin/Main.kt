b3ff9f36fda426095d501ed6a26c1d45725e07bd
Anne Jetton <annemariejetton@gmail.com>
2022-05-22 18:12:39 -0600

    basic input

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -12,16 +15,35 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
-@Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+//        place to type in shipment id
+//        track button next to id input
+//        each added shipment has its own div with
+//          shipment #
+//          x button
+//          status
+//          location
+//          expected delivery
+//          status updates
+
+        var textFieldInput by remember { mutableStateOf("") }
+
+        Row {
+            TextField(textFieldInput,
+                onValueChange = { newValue ->
+                    textFieldInput = newValue
+                }
+            )
+            Button(
+                onClick = {/*add TrackingSimulator.addShipment(id) here*/}) {
+                Text("Track")
+            }
         }
+        Column {
+//            shipments will eventually go here
+        }
+
     }
 }
 
--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -1,2 +1,6 @@
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 class TrackerViewHelper {
+//    var numColumns by remember { mutableStateOf(0) }
 }
