e6688af59eae0b64cf88e491f48add7526e96ce4
Anne Jetton <annemariejetton@gmail.com>
2022-05-23 15:09:52 -0600

    more work on the base app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,26 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.lang.reflect.Modifier
 
 @Composable
 fun App() {
     MaterialTheme {
 
         var textFieldInput by remember { mutableStateOf("") }
+//        var trackedShipments by remember { mutableStateOf(mutableListOf<Shipment>()) }
+        var messages = remember { mutableStateListOf<String>() }
 
+    Column {
         Row {
             TextField(textFieldInput,
                 onValueChange = { newValue ->
@@ -27,14 +28,40 @@ fun App() {
                 }
             )
             Button(
-                onClick = {/*add TrackingSimulator.addShipment(id) here*/}) {
+                onClick = {/*add TrackingSimulator.addShipment(id) here*/messages.add("tracking item") }) {
                 Text("Track")
+
             }
         }
-        Column {
-//            shipments will eventually go here
+        Row {
+            LazyColumn {
+    //                items(messages){ message ->
+    //                    Message(message)
+    //                }
+                items(messages) {
+                    Message(it)
+                }
+            }
         }
+    }
 
+    }
+}
+
+@Composable
+fun Message(message: String){
+    Row (modifier = androidx.compose.ui.Modifier.padding(16.dp)) {
+        Surface(elevation = 3.dp) {
+            Column{
+                Text(message)
+                Text("Id:")
+                Text("Status:")
+                Text("Location: ")
+                Text("Expected Delivery: ")
+                Text("Status Updates:")
+                Text("Notes")
+            }
+        }
     }
 }
 
--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -5,22 +5,22 @@ import androidx.compose.runtime.setValue
 
 class TrackerViewHelper (){
 
-    var shipmentId by remember { mutableStateOf("") }
-    var shipmentTotes by remember { mutableStateOf(mutableListOf<String>()) }
-    var shipmentUpdateHistory by remember { mutableStateOf(mutableListOf<String>()) }
-    var expectedShipmentDeliveryDate by remember { mutableStateOf(mutableListOf<String>()) }
-    var shipmentStatus by remember { mutableStateOf("") }
-
-    init {
-
-    }
-
-    fun trackShipment(id: String){
-
-    }
-
-    fun stopTracking(){
-
-    }
+//    var shipmentId by remember { mutableStateOf("") }
+//    var shipmentTotes by remember { mutableStateOf(mutableListOf<String>()) }
+//    var shipmentUpdateHistory by remember { mutableStateOf(mutableListOf<String>()) }
+//    var expectedShipmentDeliveryDate by remember { mutableStateOf(mutableListOf<String>()) }
+//    var shipmentStatus by remember { mutableStateOf("") }
+//
+//    init {
+//
+//    }
+//
+//    fun trackShipment(id: String){
+//
+//    }
+//
+//    fun stopTracking(){
+//
+//    }
 
 }
